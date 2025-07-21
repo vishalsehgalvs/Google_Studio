3ea52292c51ec4d006d217e0716a453228584548
vishal sehgal <vishalshgl336@gmail.com>
2025-07-21 08:53:34 +0000

    it is not displaying map only displaying 1000X500

--- src/components/dashboard/MockMap.tsx
+++ src/components/dashboard/MockMap.tsx
@@ -9,8 +9,8 @@ export default function MockMap() {
       <Image 
         src="https://placehold.co/1000x500.png"
         alt="Satellite view of a farm field"
-        layout="fill"
-        objectFit="cover"
+        fill
+        className="object-cover"
         data-ai-hint="satellite farm"
       />
        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
